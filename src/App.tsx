7557c4847328c2d19d49ad33e025f744f99eb994
MartynaStrzempek <mstrzempek@motoinsight.com>
2026-02-15 12:46:46 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <aside>
+        <a>Dashboard</a>
+        <a>Zwierzęta</a>
+      </aside>
+      <main>
+        <div>
+          <div>kafelek</div>
+          <div>kafelek</div>
+          <div>kafelek</div>
+        </div>
+
+        <div>
+          <div>wykres</div>
+          <div>wykres</div>
+          <div>wykres</div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
